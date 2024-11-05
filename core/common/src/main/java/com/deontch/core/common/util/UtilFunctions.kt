c8fcf493b19b8696a419aab44d3113e70eb83f6d
deonwaju <deonolarewaju@gmail.com>
2024-11-05 21:59:47 +0100

    Feat: Introduced core-common modules for common dependencies

---
+++ core/common/src/main/java/com/deontch/core/common/util/UtilFunctions.kt
@@ -0,0 +1,43 @@
+package com.deontch.core.common.util
+
+import android.content.Context
+import android.content.pm.PackageManager
+import timber.log.Timber
+import java.util.Locale
+
+fun getAppVersionName(context: Context): String {
+    var versionName = ""
+    try {
+        val info = context.packageManager?.getPackageInfo(context.packageName, 0)
+        versionName = info?.versionName ?: ""
+    } catch (e: PackageManager.NameNotFoundException) {
+        Timber.e(e.message)
+    }
+    return versionName
+}
+
+fun Long.formatBinarySize(): String {
+    val kiloByteAsByte = 1.0 * 1024.0
+    val megaByteAsByte = 1.0 * 1024.0 * 1024.0
+    val gigaByteAsByte = 1.0 * 1024.0 * 1024.0 * 1024.0
+    return when {
+        this < kiloByteAsByte -> "${this.toDouble()} B"
+        this >= kiloByteAsByte && this < megaByteAsByte -> "${
+            String.format(
+                Locale.getDefault(),
+                "%.2f",
+                (this / kiloByteAsByte)
+            )
+        } KB"
+
+        this >= megaByteAsByte && this < gigaByteAsByte -> "${
+            String.format(
+                Locale.getDefault(),
+                "%.2f",
+                (this / megaByteAsByte)
+            )
+        } MB"
+
+        else -> "Bigger than 1024 TB"
+    }
+}
